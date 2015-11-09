4abf0c2be0095bad42210652b65272b8aa55fb87
Joel Ng <blood_elves88@yahoo.com.sg>
2015-11-10 00:44:16 +0800

    Remove all C++ code

--- CloudyGameThinClient/Serializer.h
+++
@@ -1,7 +0,0 @@
-
-unsigned char * serialize_int8(unsigned char *buffer, int value);
-unsigned char * serialize_int16(unsigned char *buffer, int value);
-unsigned char * serialize_int32(unsigned char *buffer, int value);
-unsigned char * serialize_char(unsigned char *buffer, char value);
-
-unsigned char * serialize_PacketKeyboard(unsigned char *buffer, struct PacketKeyboard *value);
