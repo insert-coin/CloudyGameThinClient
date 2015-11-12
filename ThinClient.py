34543683b4663b520ad67472eee12031a8a85351
Joel Ng <blood_elves88@yahoo.com.sg>
2015-11-12 16:47:59 +0800

    - Optimize event polling - Set mouse visible - Enable key repeat - Add detection of mouse motion and mouse button

--- ThinClient.py
+++ ThinClient.py
@@ -1,15 +1,14 @@
 import socket
 import struct
 import pygame, time
-from pygame.locals import *
-
+from pygame.locals import * 
 
 """
 8bit Version (Currently use 0)
-8bit Type : (Keyboard, Mouse, Gamepad, etc
-32bit Sequence
-8bit ControllerID
-16bit KeyCode (A, B, , Z, 0, ... ,9, F1, , F12, etc)
+8bit Type : (Keyboard (0), Mouse (1), Gamepad, etc
+32bit Sequence (counter for event)
+8bit ControllerID (start from 0)
+16bit KeyCode (A, B, , Z, 0, ... ,9, F1, ..., F12, etc)
 8bit Event (Key Down, Key Up)
 """
 
@@ -32,23 +31,28 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((640, 480))
     pygame.display.set_caption('Remote Keyboard')
-    pygame.mouse.set_visible(0)
+    pygame.mouse.set_visible(True)
     
     isRunning = True
+    pygame.key.set_repeat(33, 33) # 1 input per frame
 
     while isRunning:
-        for event in pygame.event.get():
-            if (event.type == KEYUP or event.type == KEYDOWN):
-                if event.type == KEYDOWN and len(event.unicode) > 0:
-                    data = (VERSION, TYPE, sequence, CONTROLLER_ID, event.key, event.type, ord(event.unicode))
-                    message = struct.pack(PACKET_FORMAT, *data)
-                    print(message)
-                    sock.sendto(message, (UDP_IP, UDP_PORT))
-                    sequence += 1
-            if (event.type == QUIT):
-                isRunning = False
+        event = pygame.event.wait() # program will sleep if there are no events in the queue
+        if (event.type == KEYUP or event.type == KEYDOWN):
+            if (event.type == KEYDOWN and len(event.unicode) > 0):
+                data = (VERSION, TYPE, sequence, CONTROLLER_ID, event.key, event.type, ord(event.unicode))
+                message = struct.pack(PACKET_FORMAT, *data)
+                print(message)
+                sock.sendto(message, (UDP_IP, UDP_PORT))
+                sequence += 1
+        if (event.type == MOUSEMOTION):
+            x, y = pygame.mouse.get_rel()
+        if (event.type == MOUSEBUTTONDOWN or event.type == MOUSEBUTTONUP):
+            leftMouseButton, middleMouseButton, rightMouseButton = pygame.mouse.get_pressed()
+        if (event.type == QUIT):
+            isRunning = False
     
-    pygame.display.quit()
+    pygame.quit()
 
 if __name__ == '__main__':
     main()
