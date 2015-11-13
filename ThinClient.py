a21f1c4ac8e3de93d7676aac8b78542ca7290b00
Joel Ng <blood_elves88@yahoo.com.sg>
2015-11-13 12:37:11 +0800

    Reindent ASCII_TO_UE_KEYCODE dictionary

--- ThinClient.py
+++ ThinClient.py
@@ -4,64 +4,65 @@ import string
 import pygame, time
 from pygame.locals import * 
 
-ASCII_TO_UE_KEYCODE = {8: 8, # Backspace
-                       9: 9, # Tab
-                       13: 13, # Enter
-                       19: 19, # Pause Break
-                       27: 27, # Escape
-                       32: 32, # Space
-                       44: 44, # Comma
-                       47: 47, # Forward Slash
-                       59: 59, # Semicolon
-                       61: 61, # Equals
-                       93: 93, # Right square bracket
-                       127: 46, # Delete
-                       256: 96, # NumPad 0
-                       257: 97, # NumPad 1
-                       258: 98, # NumPad 2
-                       259: 99, # NumPad 3
-                       260: 100, # NumPad 4
-                       261: 101, # NumPad 5 
-                       262: 102, # NumPad 6 
-                       263: 103, # NumPad 7
-                       264: 104, # NumPad 8
-                       265: 105, # NumPad 9
-                       266: 110, # NumPad Decimal
-                       267: 111, # NumPad Divide
-                       268: 106, # NumPad Multiply
-                       269: 109, # NumPad Subtract
-                       270: 107, # NumPad Add
-                       273: 38, # Arrow Up
-                       274: 40, # Arrow Down
-                       275: 39, # Arrow Right
-                       276: 37, # Arrow Left
-                       277: 45, # Insert
-                       278: 35, # Home
-                       279: 36, # End
-                       280: 33, # Page Up
-                       281: 34, # Page Down
-                       282: 112, # F1
-                       283: 113, # F2
-                       284: 114, # F3
-                       285: 115, # F4
-                       286: 116, # F5
-                       287: 117, # F6
-                       288: 118, # F7
-                       289: 119, # F8
-                       290: 120, # F9
-                       291: 121, # F10
-                       292: 122, # F11
-                       293: 123, # F12
-                       300: 144, # Num Lock
-                       301: 20, # Caps Lock
-                       302: 145, # Scroll Lock
-                       303: 161, # Right Shift
-                       304: 160, # Left Shift
-                       305: 163, # Right Ctrl
-                       306: 162, # Left Ctrl
-                       307: 165, # Right Alt
-                       308: 164 # Left Alt
-                       }
+ASCII_TO_UE_KEYCODE = {
+   8: 8, # Backspace
+   9: 9, # Tab
+   13: 13, # Enter
+   19: 19, # Pause Break
+   27: 27, # Escape
+   32: 32, # Space
+   44: 44, # Comma
+   47: 47, # Forward Slash
+   59: 59, # Semicolon
+   61: 61, # Equals
+   93: 93, # Right square bracket
+   127: 46, # Delete
+   256: 96, # NumPad 0
+   257: 97, # NumPad 1
+   258: 98, # NumPad 2
+   259: 99, # NumPad 3
+   260: 100, # NumPad 4
+   261: 101, # NumPad 5 
+   262: 102, # NumPad 6 
+   263: 103, # NumPad 7
+   264: 104, # NumPad 8
+   265: 105, # NumPad 9
+   266: 110, # NumPad Decimal
+   267: 111, # NumPad Divide
+   268: 106, # NumPad Multiply
+   269: 109, # NumPad Subtract
+   270: 107, # NumPad Add
+   273: 38, # Arrow Up
+   274: 40, # Arrow Down
+   275: 39, # Arrow Right
+   276: 37, # Arrow Left
+   277: 45, # Insert
+   278: 35, # Home
+   279: 36, # End
+   280: 33, # Page Up
+   281: 34, # Page Down
+   282: 112, # F1
+   283: 113, # F2
+   284: 114, # F3
+   285: 115, # F4
+   286: 116, # F5
+   287: 117, # F6
+   288: 118, # F7
+   289: 119, # F8
+   290: 120, # F9
+   291: 121, # F10
+   292: 122, # F11
+   293: 123, # F12
+   300: 144, # Num Lock
+   301: 20, # Caps Lock
+   302: 145, # Scroll Lock
+   303: 161, # Right Shift
+   304: 160, # Left Shift
+   305: 163, # Right Ctrl
+   306: 162, # Left Ctrl
+   307: 165, # Right Alt
+   308: 164 # Left Alt
+}
 
 # Maps from ASCII 97-122 to UE KeyCode 65-90
 for i in string.ascii_lowercase:
