0883833f2e6dd1ec01e2271be4e0caf8c45cb24e
kwm <zeanne@users.noreply.github.com>
2016-02-06 11:04:44 +0800

    add POST method for sign in.
    input not validated, error message not specific.

--- CloudyLauncher.java
+++ CloudyLauncher.java
@@ -78,27 +78,55 @@ public class CloudyLauncher extends Application {
         GridPane loginInfo = new GridPane();
         loginInfo.setId("signup-panel");
 
+        Label firstName = new Label("First Name");
+        TextField firstNameInput = new TextField();
+        firstNameInput.setPromptText("Enter first name");
+        Label lastName = new Label("Last Name");
+        TextField lastNameInput = new TextField();
+        lastNameInput.setPromptText("Enter last name");
         Label username = new Label("Username");
         TextField usernameInput = new TextField();
+        usernameInput.setPromptText("Enter username");
         Label email = new Label("Email");
         TextField emailInput = new TextField();
+        emailInput.setPromptText("Enter email");
         Label password = new Label("Password");
         PasswordField passwordInput = new PasswordField();
+        passwordInput.setPromptText("Enter password");
 
-        Button loginButton = new Button("Sign Up");
-        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+        Text feedback = new Text();
+        feedback.setId("signup-feedback");
+        Button signupButton = new Button("Sign Up");
+        signupButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+                try {
+                    attemptUserRegistration(usernameInput.getText(),
+                                            passwordInput.getText(),
+                                            emailInput.getText(),
+                                            firstNameInput.getText(),
+                                            lastNameInput.getText());
+                    feedback.setText("User successfully registered.");
+                    
+                } catch (IOException e) {
+                    feedback.setText("Registration error ");
+                    
+                }
             }
         });
         
-        loginInfo.add(email, 0, 0);
-        loginInfo.add(emailInput, 1, 0);
-        loginInfo.add(username, 0, 1);
-        loginInfo.add(usernameInput, 1, 1);
-        loginInfo.add(password, 0, 2);
-        loginInfo.add(passwordInput, 1, 2);
-        loginInfo.add(loginButton, 0, 3);
+        loginInfo.add(firstName, 0, 0);
+        loginInfo.add(firstNameInput, 1, 0);
+        loginInfo.add(lastName, 0, 1);
+        loginInfo.add(lastNameInput, 1, 1);
+        loginInfo.add(email, 0, 2);
+        loginInfo.add(emailInput, 1, 2);
+        loginInfo.add(username, 0, 3);
+        loginInfo.add(usernameInput, 1, 3);
+        loginInfo.add(password, 0, 4);
+        loginInfo.add(passwordInput, 1, 4);
+        loginInfo.add(signupButton, 0, 5);
+        loginInfo.add(feedback, 1, 5);
 
         Tab signupTab = new Tab("Sign Up");
         signupTab.setClosable(false);
@@ -106,7 +134,6 @@ public class CloudyLauncher extends Application {
         signupTab.setContent(loginInfo);
 
     }    
-
            
     private void attemptAuthentication(String username, String password) throws IOException {
 
@@ -117,7 +144,6 @@ public class CloudyLauncher extends Application {
         String queryData = String.format("username=%s&password=%s", username, password);
         
         // send post request
-        connection.setDoOutput(true);
         DataOutputStream writer = new DataOutputStream(connection.getOutputStream());
 
         writer.writeBytes(queryData);
@@ -131,7 +157,31 @@ public class CloudyLauncher extends Application {
 
     }
     
-    
+ private void attemptUserRegistration(String username, String password, String email, String firstName, String lastName) throws IOException {
+
+     URL url = new URL(baseurl + "/users/");        
+     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+     
+     connection.setRequestMethod("POST");
+     String queryData = String.format("username=%s&password=%s&email=%s&first_name=%s&last_name=%s", username, password, email, firstName, lastName);
+     
+     // send post request
+     connection.setDoOutput(true);
+     DataOutputStream writer = new DataOutputStream(connection.getOutputStream());
+
+     writer.writeBytes(queryData);
+     writer.flush();
+     writer.close();
+
+     BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));        
+
+     String response;
+     while ((response = reader.readLine()) != null) {
+         System.out.println(response);
+     }
+     reader.close();
+ }
+ 
     @Override
     public void start(Stage primaryStage) throws Exception {
         
