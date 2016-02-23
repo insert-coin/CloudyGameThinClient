a18d26bdbc44bc354dba57763df0cb2e2718b2ae
kwm <zeanne@users.noreply.github.com>
2016-02-23 16:20:49 +0800

    change main layout from BorderPane to VBox

--- CloudyLauncherRestructured.java
+++ CloudyLauncherRestructured.java
@@ -1,14 +1,25 @@
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-
 import javafx.stage.Stage;
 
 public class CloudyLauncherRestructured extends Application {
+
+    private VBox rootLayout;
+    private Node manageUserPanel;
+    private VBox gameDisplayLayout;
+    private Node gameInfo;
+
+    @FXML TilePane gameRoot;
 
     @FXML
     protected void handleSignIn(ActionEvent event) {
@@ -30,11 +41,27 @@ public class CloudyLauncherRestructured extends Application {
         System.out.println("handle logout");
     }
 
+    void initialise() {
+        try {
+            rootLayout = (VBox) FXMLLoader.load(getClass().getResource("CloudyLauncher.fxml"));
+            ObservableList<Node> rChildren = rootLayout.getChildren();
+            System.out.println(rChildren);
+            manageUserPanel = rChildren.get(0);
+            gameDisplayLayout = (VBox) rChildren.get(1);
+            gameInfo = rChildren.get(2);
+
+            rChildren.remove(1, rChildren.size());
+
+        } catch (IOException e) {
+            System.out.println("Error in loading fxml file");
+        }
+    }
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("CloudyLauncher.fxml"));
-        Scene scene = new Scene(root, 300, 275);
+        initialise();
+        Scene scene = new Scene(rootLayout, 300, 275);
 
         primaryStage.setTitle("CloudyLauncher");
         primaryStage.setScene(scene);
