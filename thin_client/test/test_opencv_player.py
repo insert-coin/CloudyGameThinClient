66fe79c794c5e417c65a49b183fa141f24f61f8d
Eugene Yeo <yeo.eugene.oey@gmail.com>
2016-03-30 17:42:54 +0800

    build opencv manually in travis environment

---
+++ thin_client/test/test_opencv_player.py
@@ -0,0 +1,9 @@
+import unittest
+import cv2
+            
+class OpenCVPlayerTest (unittest.TestCase):      
+    def test_start_player(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
