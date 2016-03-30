3fa1e6684f231677da334f1250bbd32925f33ea8
Eugene Yeo <yeo.eugene.oey@gmail.com>
2016-03-31 01:05:41 +0800

    Reverting all my changes

--- thin_client/test/test_opencv_player.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import cv2
-            
-class OpenCVPlayerTest (unittest.TestCase):      
-    def test_start_player(self):
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
